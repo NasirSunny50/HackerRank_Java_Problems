913bd884ff46150a7e3e6195dbf89400e05838e6
Nasir Sunny <nasir.uddin08@northsouth.edu>
2022-10-26 11:10:46 +0600

    1 to 2 added

---
+++ src/Print_On_Screen.java
@@ -0,0 +1,11 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+//        Expected Output :
+//        Hello
+//        Alexandra Abramov
+
+public class Print_On_Screen {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello \nAlexandra Abramov");
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,17 @@
+//Write a Java program to print the sum of two numbers.
+//        Test Data:
+//        74 + 36
+//        Expected Output :
+//        110
+
+public class Sum {
+
+    public static void main (String[] args)
+    {
+        int a = 74;
+        int b = 36;
+        int sum = a+b;
+
+        System.out.println(sum);
+    }
+}
